2862913858ea9cdf8afdafe8fc440db4248794d3
Kroexov <kroexov@gmail.com>
2022-12-12 13:56:00 +0300

    finalized lab-4

---
+++ js/starter.js
@@ -0,0 +1,21 @@
+window.addEventListener('load', function () {
+    let doc = document.location.pathname.split("/").reverse()[0];
+    const mainPage = document.getElementById('mainPage');
+    const stocksPage = document.getElementById('stocksPage');
+    const advicesPage = document.getElementById('advicesPage');
+    switch (doc) {
+        case "index.html":
+            mainPage.classList.toggle('active');
+            break;
+        case "stocks.html":
+            stocksPage.classList.toggle('active');
+            break;
+        case "advices.html":
+            advicesPage.classList.toggle('active');
+            break;
+    }
+
+    var loadTime = window.performance.timing.domContentLoadedEventEnd - window.performance.timing.navigationStart;
+    const pageLoad = document.getElementById('pageLoad');
+    pageLoad.textContent = "loadtime is " + loadTime/1000 + " seconds";
+});
