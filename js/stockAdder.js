cfdaa4ce78179877adb3cc365013fecb979ef343
Kroexov <kroexov@gmail.com>
2022-12-26 13:07:00 +0300

    final fix to lab-5

--- js/stockAdder.js
+++ js/stockAdder.js
@@ -1,23 +1,33 @@
-/*(function($){})(jQuery);*/
+function GetStock(){
+    var table = document.getElementById('table');
+    var inpval = document.getElementById('name').value.toUpperCase();
+    var inpprice = document.getElementById('price').value;
+    var inpchange = document.getElementById('change').value;
+    let found = false;
+    for (i = 0; i<table.rows.length; i++){
+        if (found)
+            break;
+        if (table.rows[i].cells[0].innerText == inpval){
+            table.rows[i].cells[2].innerText = parseInt(table.rows[i].cells[2].innerText) + 1;
+            found = true;
+        }
+    }
+    if (!found){
+        var rowCount = table.rows.length;
+        var row = table.insertRow(rowCount);
+        row.insertCell(0).innerHTML = inpval;
+        row.insertCell(1).innerHTML = "₽"+inpprice;
+        row.insertCell(2).innerHTML = "1";
+        row.insertCell(3).innerHTML = inpchange + "%";
+    }
+    localStorage.setItem('val', inpval);
+    localStorage.setItem('price', inpprice);
+    localStorage.setItem('change', inpchange);
+    return false;
+}
 
-/*$(document).ready(function() {
-    setInterval(window.onload = function () {
-
-        document.getElementById('reviews').addEventListener('submit', function (e) {
-            GetStock();
-        })
-
-    });
-});*/
-
-async function GetStock(){
-    var stocks = new Stocks('2TID4B9EQDA8974C');
-    var options = {
-        symbol: 'AAPL',
-        interval: 'weekly',
-        amount: 52
-    };
-    var result = await stocks.timeSeries(options);
-    var anotherRes = 0;
-    alert(result);
+function GetSavedStock(){
+    document.getElementById('name').value = localStorage.val;
+    document.getElementById('price').value = localStorage.price;
+    document.getElementById('change').value = localStorage.change;
 }
