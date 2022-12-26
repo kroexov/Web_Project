8fda47e063387a73792cbb36c01c99a50df804bc
Kroexov <kroexov@gmail.com>
2022-12-26 13:36:37 +0300

    fix to lab-4

--- js/starter.js
+++ js/starter.js
@@ -1,6 +1,12 @@
 window.addEventListener('load', function () {
     let doc = document.location.pathname.split("/").reverse()[0];
-    const mainPage = document.getElementById('mainPage');
+    const nav = document.getElementById('nav').children[0];
+    for (i = 0; i<nav.children.length; i++){
+        if (nav.children[i].children[0].getAttribute("href") == "#"){
+            nav.children[i].classList.toggle('active');
+        }
+    }
+    /*const mainPage = document.getElementById('mainPage');
     const stocksPage = document.getElementById('stocksPage');
     const advicesPage = document.getElementById('advicesPage');
     switch (doc) {
@@ -13,7 +19,7 @@ window.addEventListener('load', function () {
         case "advices.html":
             advicesPage.classList.toggle('active');
             break;
-    }
+    }*/
 
     var loadTime = window.performance.timing.domContentLoadedEventEnd - window.performance.timing.navigationStart;
     const pageLoad = document.getElementById('pageLoad');
